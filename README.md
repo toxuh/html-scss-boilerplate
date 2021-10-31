d74258b934e7057379c9475c8a269322bc684479
Anton Zakharov <zakharov.toxa@gmail.com>
2021-10-31 11:37:24 +0100

    init commit

--- README.md
+++ README.md
@@ -1,79 +1,20 @@
-# HTML Boilerplate
+# HTML5 SCSS GULP Boilerplate.
 
-[![Website](https://img.shields.io/badge/Author-JagathJayakumar-red.svg?style=flat)](https://www.hellojagath.com)
-[![License](https://img.shields.io/badge/MIT-License-brightgreen.svg?style=flat)](https://github.com/jagathgj/html-sass-bootstrap-gulp-multipage-boilerplate/blob/master/LICENSE.md)
-[![Version](https://img.shields.io/badge/Version-1.0.0-blueviolet.svg?style=flat)](https://github.com/jagathgj/html-sass-bootstrap-gulp-multipage-boilerplate)
+This is the deep refactoring of Jagath Jayakumar [repo](https://github.com/jagathgj/html-sass-bootstrap-gulp-multipage-boilerplate).
 
-<p align="center">
-  <img src="./src/assets/img/banner.PNG" />
-</p>
+#### Changelog:
 
-## Intro
+* Project is now supports ES6 (module).
+* All libraries are up-to-date.
+* Removed bootstrap (bootstrap 5 will be added later).
+* Fontawesome included like js script
+* Scripts are now in partial.
+* Project build is now in different folder (/build) and all minifiers works correct.
+* Added editor config for some codestyle.
 
-The best starter kit for website with multipages using Bootstrap 4, Gulp 4, Sass, BrowserSync with minification.
+#### In progress:
 
-## Getting Started
-
-Below instructions will help you get a copy of the project up and running on your local machine for starting multipage html development.
-
-### Prerequisites
-
-Install gulp globally on your system
-
-```
-npm install gulp -g
-```
-
-### Cloning
-
-Clone project from github Repository
-
-```
-$ git clone https://github.com/jagathgj/html-sass-bootstrap-gulp-multipage-boilerplate.git
-```
-
-### Installing
-
-Then Install the packages included in *package.json*
-
-```
-$ npm install
-```
-
-### Build
-
-Build for generating pure css/js/fonts/html files in public folder, which is the one to deploy
-
-```
-$ npm run build
-```
-
-### Run the project - Live on browser
-
-For live development and run the project in browser
-
-```
-$ npm run start
-```
-
-## Built With
-
-* [Bootstrap 4](https://getbootstrap.com/)
-* [Gulp 4](https://gulpjs.com/)
-* [Sass](https://sass-lang.com/)
-
-## Authors
-
-* **Jagath Jayakumar** - [Github](https://github.com/jagathgj) - [Official](https://www.hellojagath.com)
-
-## License
-
-This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
-
-## Acknowledgments
-
-* Hat tip to anyone whose code was used
-* Inspiration
-* Myself
-* etc
-
+* fix all vulnerabilities in libraries
+* front page
+* about page
+* readme update
--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,112 @@
-const gulp = require('gulp');
-const gulpIf = require('gulp-if');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const htmlmin = require('gulp-htmlmin');
-const cssmin = require('gulp-cssmin');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const concat = require('gulp-concat');
-const jsImport = require('gulp-js-import');
-const sourcemaps = require('gulp-sourcemaps');
-const htmlPartial = require('gulp-html-partial');
-const clean = require('gulp-clean');
-const googleWebFonts = require( 'gulp-google-webfonts' );
-const cssbeautify = require('gulp-cssbeautify');
-const htmlbeautify = require('gulp-html-beautify')
-const isProd = process.env.NODE_ENV === 'prod';
+import browserSync from 'browser-sync';
 
-var options = { };
+import gulp from 'gulp';
+import gulpIf from 'gulp-if';
+import clean from 'gulp-clean';
+
+import htmlPartial from 'gulp-html-partial';
+import htmlMin from 'gulp-htmlmin';
+import htmlBeautify from 'gulp-html-beautify';
+
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cssMin from 'gulp-cssmin';
+import cssBeautify from 'gulp-cssbeautify';
+
+import jsImport from 'gulp-js-import';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
+
+import gulpImagemin from 'gulp-imagemin';
+
+browserSync.create();
+
+const isProd = process.env.NODE_ENV === 'prod';
+const sass = gulpSass(dartSass);
+const dir = isProd ? 'build' : 'public';
 
 const htmlFile = [
-    'src/*.html'
+  'src/*.html'
 ]
 
-function html() {
-    return gulp.src(htmlFile)
-        .pipe(htmlPartial({
-            basePath: 'src/assets/partials/'
-        }))
-        .pipe(htmlbeautify())
-        .pipe(gulpIf(isProd, htmlmin({
-            collapseWhitespace: true
-        })))
-        .pipe(gulp.dest('public'));
+export const html = () => {
+  return gulp.src(htmlFile)
+    .pipe(htmlPartial({
+      basePath: 'src/assets/partials/'
+    }))
+    .pipe(htmlBeautify())
+    .pipe(gulpIf(isProd, htmlMin({
+      collapseWhitespace: true
+    })))
+    .pipe(gulp.dest(dir));
 }
 
-function css() {
-    return gulp.src('src/assets/sass/style.scss')
-        .pipe(gulpIf(!isProd, sourcemaps.init()))
-        .pipe(sass({
-            includePaths: ['node_modules']
-        }).on('error', sass.logError))
-        .pipe(cssbeautify({
-            indent: '  ',
-            openbrace: 'separate-line',
-            autosemicolon: true
-        }))
-        .pipe(gulpIf(!isProd, sourcemaps.write()))
-        .pipe(gulpIf(isProd, cssmin()))
-        .pipe(gulp.dest('public/assets/css/'));
+export const css = () => {
+  return gulp.src('src/assets/sass/styles.scss')
+    .pipe(gulpIf(!isProd, sourcemaps.init()))
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(cssBeautify({
+      indent: '  ',
+      openbrace: 'separate-line',
+      autosemicolon: true
+    }))
+    .pipe(gulpIf(!isProd, sourcemaps.write()))
+    .pipe(gulpIf(isProd, cssMin({
+      processImport: false,
+    })))
+    .pipe(gulp.dest(`${dir}/assets/css/`));
 }
 
-function js() {
-    return gulp.src('src/assets/js/*.js')
-        .pipe(jsImport({
-            hideConsole: true
-        }))
-        // .pipe(concat('all.js'))
-        .pipe(gulpIf(isProd, uglify()))
-        .pipe(gulp.dest('public/assets/js'));
+export const js = () => {
+  return gulp.src('src/assets/js/*.js')
+    .pipe(jsImport({
+      hideConsole: true
+    }))
+    .pipe(gulpIf(isProd, uglify()))
+    .pipe(gulp.dest(`${dir}/assets/js`));
 }
 
-function img() {
-    return gulp.src('src/assets/img/*')
-        .pipe(gulpIf(isProd, imagemin()))
-        .pipe(gulp.dest('public/assets/img/'));
+const img = () => {
+  return gulp.src('src/assets/img/*')
+    .pipe(gulpIf(isProd, gulpImagemin()))
+    .pipe(gulp.dest(`${dir}/assets/img/`));
 }
 
-function fonts() {
-    return gulp.src('src/assets/fonts/*.{eot,svg,ttf,woff,woff2}')
-		.pipe(gulp.dest('public/assets/fonts/'));
-}
-
-function fontAwesome() {
-    return gulp.src('./node_modules/@fortawesome/**/*')
-		.pipe(gulp.dest('public/assets/vendor/'));
+export const fonts = () => {
+  return gulp.src('src/assets/fonts/*.{eot,svg,ttf,woff,woff2}')
+    .pipe(gulp.dest(`${dir}/assets/fonts/`));
 }
 
 
-function serve() {
-    browserSync.init({
-        open: true,
-        notify: false,
-        server: './public'
-    });
+function serveInner() {
+  browserSync.init({
+    open: true,
+    notify: false,
+    server: './public'
+  });
 }
 
 function browserSyncReload(done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 }
 
 
-function watchFiles() {
-    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
-    gulp.watch('src/assets/**/*.scss', gulp.series(css, browserSyncReload));
-    gulp.watch('src/assets/**/*.js', gulp.series(js, browserSyncReload));
-    gulp.watch('src/assets/img/**/*.*', gulp.series(img));
-    gulp.watch('src/assets/**/*.{eot,svg,ttf,woff,woff2}', gulp.series(fonts));
-    gulp.watch('src/assets/vendor/**/*.*', gulp.series(fontAwesome));
+const watchFiles = () => {
+  gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
+  gulp.watch('src/assets/**/*.scss', gulp.series(css, browserSyncReload));
+  gulp.watch('src/assets/**/*.js', gulp.series(js, browserSyncReload));
+  gulp.watch('src/assets/img/**/*.*', gulp.series(img));
+  gulp.watch('src/assets/**/*.{eot,svg,ttf,woff,woff2}', gulp.series(fonts));
 
-    return;
+  return;
 }
 
-function del() {
-    return gulp.src('public/*', {read: false})
-        .pipe(clean());
+export const del = () => {
+  return gulp.src(`${dir}/*`, {read: false})
+    .pipe(clean());
 }
 
-exports.css = css;
-exports.html = html;
-exports.js = js;
-exports.fonts = fonts;
-exports.fontAwesome = fontAwesome;
-exports.del = del;
-exports.serve = gulp.parallel(html, css, js, img, fonts, fontAwesome, watchFiles, serve);
-exports.default = gulp.series(del, html, css, js, fonts, img, fontAwesome);
+export const serve = gulp.parallel(html, css, js, img, fonts, watchFiles, serveInner)
+
+export default gulp.series(del, html, css, js, fonts, img)
